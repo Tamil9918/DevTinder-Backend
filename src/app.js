0a962f62080b10b2a21222dae2e102c27dbb2885
Tamil9918 <tstamil9918@gmail.com>
2024-11-19 22:08:07 +0530

    initiated the express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("Starting new project");
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `devTinder Backend Server is listening at port:${process.env.PORT}`
+  );
+});
