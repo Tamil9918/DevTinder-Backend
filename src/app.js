4f51858a1373c9a6c6906a30b41cee791e7ab5c7
Tamil9918 <tstamil9918@gmail.com>
2024-11-22 20:49:46 +0530

    test

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello!");
-});
+app.use(
+  "/test",
+  (req, res, next) => {
+    next();
+  },
+  (req, res) => {
+    res.send("hi");
+  }
+);
 
 app.listen(process.env.PORT, () => {
   console.log(
-    `devTinder Backend Server is listening at port:${process.env.PORT}`
+    `DevTinder Backend Server is listening at port:${process.env.PORT}`
   );
 });
