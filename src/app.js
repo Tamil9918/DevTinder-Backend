0525bf5220213c7aa506b33cc15b2bb3d9e36396
Tamil9918 <tstamil9918@gmail.com>
2024-11-24 20:58:19 +0530

    added apis

--- src/app.js
+++ src/app.js
@@ -3,22 +3,69 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
-
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Tamil",
-    lastName: "Selvan",
-    emailId: "tamil@gmail.com",
-    password: "test123",
-  }; //creating a new instance of user model
-  const user = new User(userObj);
   try {
+    const user = new User(req.body);
     await user.save();
     res.send("User added successfully...");
   } catch (error) {
-    res.status(400).send("Error saving record to db", error.message);
+    res.status(400).send("Error saving record to db" + error.message);
   }
 });
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error saving record to db" + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.findOne({ emailId: req.body.email });
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Error saving record to db" + error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = await User.findByIdAndDelete({ _id: req.body.userId });
+    if (!userId) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User Deleted Successfully...");
+    }
+  } catch (error) {
+    res.status(400).send("Error saving record to db" + error.message);
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = await User.findOneAndUpdate(
+      { _id: req.body.userId },
+      req.body,
+      { returnDocument: "after" }
+    );
+    if (!userId) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User Updated Successfully...");
+    }
+  } catch (error) {
+    res.status(400).send("Error saving record to db" + error.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database cluster connection established...");
