6de952790b323e20c8c2cac4909093c89184d844
Tamil9918 <tstamil9918@gmail.com>
2024-12-03 15:36:20 +0530

    apis added

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -9,6 +9,7 @@ const connnectionRequestSchema = new mongoose.Schema(
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     status: {
--- src/routes/user.js
+++ src/routes/user.js
@@ -27,8 +27,15 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", ["firstName", "lastName"]);
-    const data = connectionRequest.map((r) => r.fromUserId);
+    })
+      .populate("fromUserId", ["firstName", "lastName"])
+      .populate("toUserId", ["firstName", "lastName"]);
+    const data = connectionRequest.map((r) => {
+      if (r.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return r.toUserId;
+      }
+      return r.fromUserId;
+    });
     res.json({ message: "Data fetched successfully", data });
   } catch (error) {
     res.status(400).send("Error:" + error.message);
